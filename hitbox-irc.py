a2402ba331c02f2eca6b2f5f9c82e57f6cb84306
esi <xpmaster2k4@gmail.com>
2015-01-19 03:44:57 -0500

    barebones irc server + join/message capability
    need to implement username indicator + basically everything else still but good start

--- hitbox-irc.py
+++ hitbox-irc.py
@@ -1,17 +1,105 @@
-import json, random, requests, time
+import json, random, requests, socket, time
 from ws4py.client.threadedclient import WebSocketClient
+
+pendingMessages = ''
 
 class LoginException(BaseException):
 	pass
 
+class IRCServer:
+	def __init__(self):
+		self._connection = None
+		self._connected = False
+		self._negotiated = False
+		self._hitboxChat = None
+		self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self._socket.bind(('', 7778))
+		self._socket.listen(1)
+		self._username = None
+		self._password = None
+		self._HandleClients()
+
+	def _HandleClients(self):
+		global pendingMessages
+		while True:
+			self._connection, client_address = self._socket.accept()
+			try:
+				client_address
+				self._connected = True
+
+				while self._connected:
+					data = self._connection.recv(4096).decode('UTF-8').split('\r\n')[:-1]
+					for line in data:
+						print(''.join(['< ', line]))
+						line = line.split(' ')
+						if line[0] == 'NICK':
+							self._username = line[1]
+						elif line[0] == 'PASS':
+							self._password = line[1]
+						elif line[0] == 'USER':
+							if self._username == None:
+								pass
+							elif self._password == None:
+								self._SendServerMessageToClient('464 %s :No password given' % self._username)
+								self._connected = False
+							else:
+								self._hitboxChat = HitboxSocket()
+								self._SendServerMessageToClient('NOTICE :IRC connected, logging into Hitbox...')
+								try:
+									self._hitboxChat.authenticate(self._username, self._password)
+								except LoginException:
+									self._SendServerMessageToClient('464 %s :Password given is invalid' % self._username)
+									self._connected = False
+									break
+								self._SendServerMessageToClient('NOTICE :Login successful, connecting to chat...')
+								self._hitboxChat.connect()
+								self._SendServerMessageToClient('NOTICE :Connection successful!')
+								self._negotiated = True
+						elif line[0] == 'QUIT':
+							self._connected = False
+							self._negotiated = False
+						elif self._negotiated == True:
+							if line[0] == 'JOIN':
+								self._hitboxChat.join(line[1][1:])
+								self._SendMessageToClient('JOIN %s' % line[1])
+							elif line[0] == 'PRIVMSG':
+								self._hitboxChat.privmsg(line[1][1:], ' '.join(line[2:])[1:])
+
+					if self._negotiated:
+						if pendingMessages != '':
+							pendingMessages = pendingMessages.split('\r\n')[:-1]
+							for line in pendingMessages:
+								print(line)
+								line = line[4:]
+								line = json.loads(line)
+
+								if line['args']['method'] == 'chatMsg':
+									self._SendMessageToClient('PRIVMSG #%s :%s' % (line['args']['param']['channel'], line['args']['param']['text']))
+							pendingMessages = ''
+
+			finally:
+				self._connection.close()
+
+	def _SendServerMessageToClient(self, message):
+		print(''.join(['> ', ':hitbox-irc-proxy ', message]))
+		self._connection.sendall(''.join([':hitbox-irc-proxy ', message, '\r\n']).encode('UTF-8'))
+
+	def _SendMessageToClient(self, message):
+		hostmask = ''.join([self._username, '!', self._username, '@hitbox-irc-proxy'])
+		print(''.join(['> ', ':', hostmask, ' ', message]))
+		self._connection.sendall(''.join([':', hostmask, ' ', message, '\r\n']).encode('UTF-8'))
+
 class HitboxSocket:
 	class _HitboxWS(WebSocketClient):
 		def received_message(self, message):
+			global pendingMessages
 			if str(message) == '1::':
 				print('~ CONNECTED')
 			elif str(message) == '2::':
 				self.send('2::')
 			else:
+				pendingMessages = ''.join([pendingMessages, str(message)])
 				print(''.join(['< ', str(message)[3:]]))
 
 	def __init__(self):
@@ -89,14 +177,5 @@ class HitboxSocket:
 		j = json.dumps(query)
 		self._SendMessage(j)
 
-chat = HitboxSocket()
-try:
-	chat.authenticate('esi', 'password')
-except LoginException as e:
-	print(e)
-	exit()
-chat.connect()
-chat.join('esi')
-time.sleep(2)
-chat.privmsg('esi', 'it works!')
-chat._socket.run_forever()
+
+IRCServer()
